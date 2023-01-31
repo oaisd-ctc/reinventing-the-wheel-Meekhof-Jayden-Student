5350c62a02851a844c937bb3f5de7ff7c585cb08
Meekhof-Jayden-Student <112406667+Meekhof-Jayden-Student@users.noreply.github.com>
2023-01-31 13:39:55 +0000

    .

--- utils/StringUtils.cs
+++ utils/StringUtils.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace MyUlilities
+{
+
+    public static class StringUtils
+    {
+        public static string Uppercase(string s)
+    {
+    return s.ToUpper();
+    }
+
+    public static string Lowercase(string s)
+    {
+    return s.ToLower();
+    }
+    
+    }
+}
